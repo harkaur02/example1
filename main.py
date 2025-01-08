3723174f07504feefbe405f1c0bb27126ed0f1e2
Harjit Kaur <harkaur02@gmail.com>
2025-01-08 11:53:22 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+print("This progrsm generates randon numbers between 0 & 99")
+print(random.randint(0,99))
